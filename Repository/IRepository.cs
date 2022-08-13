67c89cf9acda7957b65b85191572041e40bf6195
Pham Dac Tuan <phamdactuan@laodong.com.vn>
2022-08-13 14:27:55 +0700

    add project authenticationRepository

---
+++ Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationRepository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+        T Get(int id, bool Status = true);
+        void Insert(T entity, bool saveChange = true);
+        void Update(T entity, bool saveChange = true);
+        void Delete(T entity, bool saveChange = true);
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,90 @@
+﻿using DataAccess;
+using DataAccess.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationRepository
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly AppDBContext _context;
+        private DbSet<T> entities;
+        string errorMessage = string.Empty;
+
+        public Repository(AppDBContext _context)
+        {
+            this._context = _context;
+            entities = _context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return entities.AsQueryable();
+        }
+
+        public T Get(int id, bool Status = true)
+        {
+            return entities.FirstOrDefault(x => x.Id == id && (x.Status || !Status));
+        }
+
+        public IEnumerable<T> Filter(Expression<Func<T, bool>> filter)
+        {
+            return entities.Where(filter);
+        }
+
+        public void Insert(T entity, bool saveChange = true)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entity.CreatedTime = DateTime.Now;
+            entity.UpdatedTime = DateTime.Now;
+
+            entities.Add(entity);
+
+            if (saveChange)
+            {
+                _context.SaveChanges();
+            }
+        }
+
+        public void Update(T entity, bool saveChange = true)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+            entity.UpdatedTime = DateTime.Now;
+
+            entities.Update(entity);
+
+            if (saveChange)
+            {
+                _context.SaveChanges();
+            }
+        }
+
+        public void Delete(T entity, bool saveChange = true)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+
+
+            entities.Remove(entity);
+
+            if (saveChange)
+            {
+                _context.SaveChanges();
+            }
+        }
+    }
+}
